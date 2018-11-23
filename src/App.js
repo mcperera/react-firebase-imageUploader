13e46f584db6e483a6d415fa3a34e6e12e337dcf
Madushan Perera <madushanperera1212@gmail.com>
2018-11-23 20:00:55 +0530

    Firebase Image Uploader with React.js

--- src/App.js
+++ src/App.js
@@ -1,27 +1,57 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import fireStorage from './firebase';
+import FileUploader from 'react-firebase-file-uploader';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+  constructor(){
+    super();
+    this.state={
+      avatar: "",
+      isUploading: false,
+      progress: 0,
+      avatarURL: ""
+    }
+  }
+
+  handleUploadStart = () => this.setState({ isUploading: true, progress: 0 });
+
+  handleProgress = progress => this.setState({ progress });
+
+  handleUploadError = error => {
+    this.setState({ isUploading: false });
+    console.error(error);
+  };
+
+  handleUploadSuccess = filename => {
+    this.setState({ avatar: filename, progress: 100, isUploading: false });
+    fireStorage.storage().ref("my_photos")
+      .child(filename)
+      .getDownloadURL()
+      .then(url => this.setState({ avatarURL: url }));
+  };
+
+  render(){
+    return(
+      <div id="imageUploder">
+        <h1>Code Test <br/> Image Uploader</h1>
+        <form>
+          <FileUploader
+            storageRef={
+              fireStorage.storage().ref('my_photos')
+            }
+            onUploadStart = {this.handleUploadStart}
+            onUploadError = {this.handleUploadError}
+            onUploadSuccess = {this.handleUploadSuccess}
+            onProgress = {this.handleProgress}
+          />
+        </form>
+        <p>Process: {this.state.progress}%</p>
+        <div className = "imageViewer">
+          <img src={this.state.avatarURL} alt='' style={{width:'400px', height:'400px'}}/>
+        </div>
       </div>
-    );
+    )
   }
 }
 
---
+++ src/firebase.js
@@ -0,0 +1,7 @@
+import firebase from 'firebase';
+
+var config = {
+      // YOUR FIREBASE DETAILS
+  };
+  const fireStorage = firebase.initializeApp(config);
+  export default fireStorage;
